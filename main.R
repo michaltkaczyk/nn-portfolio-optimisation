d6ead61ae6b21ebbd3276fd494caa5715b673641
michaltkaczyk <michaltkaczyk@outlook.com>
2021-01-26 15:06:19 +0100

    Create main.R
    add some basic functionality

---
+++ main.R
@@ -0,0 +1,22 @@
+library(tidyverse)
+
+read_nn_csv <- function(file) {
+    read_csv(file) %>% 
+        rename(date = Data) %>% 
+        select(-Waluta)
+}
+
+funds <- list(
+    read_nn_csv("./historical-fund-data/NN_Akcji_(A)_1998-03-11_2021-01-07.txt"),
+    read_nn_csv("./historical-fund-data/NN_Obligacji_(A)_1999-02-25_2021-01-07.txt"))
+
+min_date <- min(c(funds[[1]]$date, funds[[2]]$date))
+max_date <- max(c(funds[[1]]$date, funds[[2]]$date))
+
+funds_data <-
+    tibble(date = seq(min_date, max_date, "days")) %>% 
+    left_join(funds[[1]]) %>% 
+    left_join(funds[[2]]) %>%
+    gather("fund", "value", -date)
+
+ggplot(funds_data, aes(x = date, y = value, color = fund)) + geom_line()

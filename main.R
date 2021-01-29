ef83385cddd343672992a81a39289c7400284ad9
michaltkaczyk <michaltkaczyk@outlook.com>
2021-01-29 19:21:10 +0100

    Add start and end date to analysis

--- main.R
+++ main.R
@@ -1,14 +1,27 @@
 library(tidyverse)
+library(lubridate)
 
 source("utils.R")
 
 HISTORICAL_DATA_PATH <- "./historical-fund-data"
 
-funds <- HISTORICAL_DATA_PATH %>% 
+START_DAY <- ymd("2005-01-01")
+END_DAY <-  ymd("2020-12-31")
+
+funds_data <- HISTORICAL_DATA_PATH %>% 
     list_nn_funds() %>% 
     bind_rows()
 
-ggplot(funds, aes(x = date, y = value, color = fund)) +
+funds_old_enough <- funds_data %>%
+    group_by(fund) %>%
+    summarise(start_day = min(date)) %>% 
+    filter(start_day <= START_DAY) %>% 
+    pull(fund)
+
+funds_data <- funds_data %>%
+    filter(fund %in% funds_old_enough & date >= START_DAY & date <= END_DAY)
+
+ggplot(funds_data, aes(x = date, y = value, color = fund)) +
     geom_line() +
     ggtitle("NN Investment Funds") +
     xlab("Time") +
